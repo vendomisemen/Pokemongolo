2d5284af6ea25cbc1e3c59350b56528c40ccd1fb
vendomisemen <jpuj91@gmail.com>
2016-07-29 23:43:48 +0200

    Create AssemblyInfo.cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("PokemonGo.RocketAPI.Window")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PokemonGo.RocketAPI.Window")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c85b9d78-9ca3-4830-84cd-6b69090b987f")]
+[assembly: AssemblyVersion("1.7.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
